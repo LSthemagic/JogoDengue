3a42fcfc40bda2e4ee04b374aaf7af2c3a6fa3d4
Railan Santana <lansilvah14fsa@gmail.com>
2024-04-18 18:36:45 -0300

    atualização de mapa e scenes

--- Character/scripts/character.gd
+++ Character/scripts/character.gd
@@ -62,3 +62,8 @@ func _animate() -> void:
 func _on_attack_timer_timeout():
 	set_physics_process(true)
 	_is_attacking = false
+
+
+func _on_attack_area_body_entered(body):
+	if body.is_in_group("enemy"):
+		body.update_health(randi_range(1,5))
